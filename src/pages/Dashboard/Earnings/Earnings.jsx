4b36e4c3e2b13a829de845855c34324ff812561a
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-12-12 20:17:58 +0600

    add Decorator earnings Page and functionality

--- src/pages/Dashboard/Earnings/Earnings.jsx
+++ src/pages/Dashboard/Earnings/Earnings.jsx
@@ -33,6 +33,11 @@ const EarningsPage = () => {
     return <LoadingSpinner />;
   }
 
+
+
+
+
+  
   return (
     <div className="min-h-screen px-4 md:px-6 py-10 ">
       <h1 className="text-3xl font-bold mb-6">My Earnings</h1>
